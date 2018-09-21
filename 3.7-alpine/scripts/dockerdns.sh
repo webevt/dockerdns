950e67ef4a69ac73b9396a979e2c164ba957fb79
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2018-09-21 16:21:24 +0300

    - added cleanup after exit

--- 3.7-alpine/scripts/dockerdns.sh
+++ 3.7-alpine/scripts/dockerdns.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 
-resolvconf_file="${RESOLVCONF:-/tmp/resolv.conf}"
+resolvconf_file="${RESOLVCONF:-/tmp/resolv.conf}";
+dockerdns_mark="# added by dockerdns";
 
-function update_resolvconf () {
-    local container_ip=${CONTAINER_IP:-$(hostname -i)}
-    local dockerdns_mark="# added by dockerdns"
-
-    # Remove row with dockerdns mark.
-    echo $(cat "${resolvconf_file}" | grep -v "${dockerdns_mark}") > "${resolvconf_file}"
-
-    # Prepend row with dockerdns mark.
-    echo -e "nameserver ${container_ip} ${dockerdns_mark}\n$(cat "${resolvconf_file}")" > ${resolvconf_file}
-
-    echo "[$(date)] ${resolvconf_file} is updated"
+function cleanup () {
+    while cat "${resolvconf_file}" | grep -q "${dockerdns_mark}"; do
+        echo -e "/${dockerdns_mark}/d\nw" | ed "${resolvconf_file}" > /dev/null;
+    done
+echo
 }
 
-trap 'exit 0' SIGTERM
+function update_resolvconf () {
+    local container_ip=${CONTAINER_IP:-$(hostname -i)};
 
-update_resolvconf
+    # Remove rows with dockerdns mark.
+    cleanup || \
+        echo "Failed to cleanup ${resolvconf_file}" 1>&2;
+
+    # Prepend row with dockerdns mark.
+    echo -e "0a\nnameserver ${container_ip} ${dockerdns_mark}\n.\n,\nw" | ed "${resolvconf_file}" > /dev/null;
+
+    echo "[$(date)] ${resolvconf_file} is updated";
+}
+
+trap 'cleanup; exit 0' SIGTERM EXIT;
+
+update_resolvconf;
 while inotifywait -qq -emodify "${resolvconf_file}"; do update_resolvconf; done &
 
 socat -T15 udp4-recvfrom:53,reuseaddr,fork udp:127.0.0.11:53 &
 
-wait
+wait;
